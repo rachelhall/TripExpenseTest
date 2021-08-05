ea2703b9df77078a949f831b745b6d8177977e73
Thomas Harbin <thharbin@gmail.com>
2021-08-05 10:19:07 -0500

    comment

--- layout.js
+++ layout.js
@@ -1,14 +1,16 @@
-console.log('testing github');
+console.log("testing github");
 
-const btn = document.querySelector('.form__btn');
-btn.addEventListener('click', newInput);
+// ya boi tom wuz here
+
+const btn = document.querySelector(".form__btn");
+btn.addEventListener("click", newInput);
 
 function newInput() {
-    const form = document.querySelector('.form__list--location');
-    const newInput = document.createElement('li');
-    newInput.classList.add('form__list__item');
-    newInput.innerHTML = `
+  const form = document.querySelector(".form__list--location");
+  const newInput = document.createElement("li");
+  newInput.classList.add("form__list__item");
+  newInput.innerHTML = `
     <input type="text" placeholder="New Location" class='form__input loc_input autocomplete'>
 `;
-    form.appendChild(newInput);
+  form.appendChild(newInput);
 }
